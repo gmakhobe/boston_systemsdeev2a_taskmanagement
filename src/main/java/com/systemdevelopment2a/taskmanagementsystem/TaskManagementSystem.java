331c2a461ac9e7c2749e65b5a2724b03530fa0c5
Given Makhobela <nzilanegiven@gmail.com>
2024-10-20 02:54:00 +0200

    first commit

---
+++ src/main/java/com/systemdevelopment2a/taskmanagementsystem/TaskManagementSystem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.systemdevelopment2a.taskmanagementsystem;
+
+/**
+ *
+ * @author nzila
+ */
+public class TaskManagementSystem {
+
+    public static void main(String[] args) {
+        TaskManagementForm frm = new TaskManagementForm();
+        frm.show();
+    }
+}
